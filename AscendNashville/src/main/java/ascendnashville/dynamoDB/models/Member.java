fb49cbd16d1070450c7b5f221fa1cb1d24eb6bda
steve_winters <steve.winters85@gmail.com>
2023-01-30 11:09:03 -0600

    WIP

--- AscendNashville/src/main/java/ascendnashville/dynamoDB/models/Member.java
+++ AscendNashville/src/main/java/ascendnashville/dynamoDB/models/Member.java
@@ -1,4 +1,71 @@
 package ascendnashville.dynamoDB.models;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import java.util.List;
+
+@DynamoDBTable(tableName = "members")
 public class Member {
+    private String memberId;
+    private String name;
+    private Integer age;
+    private String gender;
+    private List<String> contactInfo;
+    private String type;
+
+    @DynamoDBHashKey(attributeName = "memberId")
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    @DynamoDBAttribute(attributeName = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @DynamoDBAttribute(attributeName = "age")
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @DynamoDBAttribute(attributeName = "gender")
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @DynamoDBAttribute(attributeName = "contactInfo")
+    public List<String> getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(List<String> contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    @DynamoDBAttribute(attributeName = "type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
