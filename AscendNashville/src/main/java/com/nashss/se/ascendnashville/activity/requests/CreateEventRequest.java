4c9d9cd33b6784eae46d92fc0e704d16d7364fb5
steve_winters <steve.winters85@gmail.com>
2023-02-12 12:22:49 -0600

    Building events classes, WIP

--- AscendNashville/src/main/java/com/nashss/se/ascendnashville/activity/requests/CreateEventRequest.java
+++ AscendNashville/src/main/java/com/nashss/se/ascendnashville/activity/requests/CreateEventRequest.java
@@ -1,4 +1,82 @@
 package com.nashss.se.ascendnashville.activity.requests;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import java.util.Date;
+
+/**
+ * Instantiates a new CreateMemberRequest object.
+ *
+ */
+@JsonDeserialize(builder = CreateEventRequest.Builder.class)
 public class CreateEventRequest {
+    private final String eventId;
+    private final Date date;
+    private final String eventDetails;
+
+    /**
+     * Instantiates a new CreateEventRequest object.
+     *
+     * @param eventId An eventId tied to an event.
+     * @param date The date of the event.
+     * @param eventDetails Event details submitted by user.
+     */
+    public CreateEventRequest(String eventId, Date date, String eventDetails) {
+        this.eventId = eventId;
+        this.date = date;
+        this.eventDetails = eventDetails;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getEventDetails() {
+        return eventDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateEventRequest{" +
+                "eventId='" + eventId + '\'' +
+                ", date=" + date +
+                ", eventDetails='" + eventDetails + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @JsonPOJOBuilder
+    public static class Builder{
+        private String eventId;
+        private Date date;
+        private String eventDetails;
+
+        public Builder withEventId(String eventId) {
+            this.eventId = eventId;
+            return this;
+        }
+
+        public Builder withDate(Date date) {
+            this.date = date;
+            return this;
+        }
+
+        public Builder withEventDetails(String eventDetails) {
+            this.eventDetails = eventDetails;
+            return this;
+        }
+
+        public CreateEventRequest build() {
+            return new CreateEventRequest(eventId, date, eventDetails);
+        }
+    }
 }
--- AscendNashville/src/main/java/com/nashss/se/ascendnashville/activity/results/CreateEventResult.java
+++ AscendNashville/src/main/java/com/nashss/se/ascendnashville/activity/results/CreateEventResult.java
@@ -1,4 +1,45 @@
 package com.nashss.se.ascendnashville.activity.results;
 
+import com.nashss.se.ascendnashville.models.EventModel;
+
+/**
+ * CreateEventResult Class.
+ */
 public class CreateEventResult {
+
+    private final EventModel event;
+
+    /**
+     * Constructor for CreateEventResult.
+     *
+     * @param event to covert.
+     */
+    private CreateEventResult(EventModel event) {
+        this.event = event;
+    }
+
+    public EventModel getEvent() {
+        return event;
+    }
+
+    @Override
+    public String toString() {
+        return "CreateEventResult{" +
+                "event=" + event +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() { return new Builder();}
+
+    public static class Builder {
+        private EventModel event;
+
+        public Builder withEvent(EventModel event) {
+            this.event = event;
+            return this;
+        }
+
+        public CreateEventResult build() { return new CreateEventResult(event);}
+    }
 }
