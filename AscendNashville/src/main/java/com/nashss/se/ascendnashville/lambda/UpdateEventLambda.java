100674e5191fe7a8c55b2c865976ec1d107ab725
steve_winters <steve.winters85@gmail.com>
2023-02-14 09:58:34 -0600

    WIP

--- AscendNashville/src/main/java/com/nashss/se/ascendnashville/lambda/UpdateEventLambda.java
+++ AscendNashville/src/main/java/com/nashss/se/ascendnashville/lambda/UpdateEventLambda.java
@@ -5,9 +5,18 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.nashss.se.ascendnashville.activity.requests.UpdateEventRequest;
 import com.nashss.se.ascendnashville.activity.results.UpdateEventResult;
 
+/**
+ * Generates a LambdaResponse.
+ */
 public class UpdateEventLambda extends LambdaActivityRunner<UpdateEventRequest, UpdateEventResult>
         implements RequestHandler<AuthenticatedLambdaRequest<UpdateEventRequest>, LambdaResponse> {
 
+    /**
+     *
+     * @param input The Authenticated Lambda Function input
+     * @param context The Lambda execution environment context object.
+     * @return LambdaResponse
+     */
     @Override
     public LambdaResponse handleRequest(AuthenticatedLambdaRequest<UpdateEventRequest> input, Context context) {
         return super.runActivity(
